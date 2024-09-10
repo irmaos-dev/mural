310ad891f7e2b41065efa9ac88b80f553199f5b7
Elivandro <Elivandro@hotmail.com.br>
2024-09-10 11:52:30 -0400

    fix: login test return

--- server/tests/Feature/Api/Auth/LoginTest.php
+++ server/tests/Feature/Api/Auth/LoginTest.php
@@ -33,7 +33,10 @@ class LoginTest extends TestCase
                 $json->has('user', fn (AssertableJson $item) =>
                     $item->whereType('token', 'string')
                         ->whereAll([
+                            'username' => $user->username,
                             'email' => $user->email,
+                            'bio' => $user->bio,
+                            'image' => $user->image,
                         ])
                 )
             );
