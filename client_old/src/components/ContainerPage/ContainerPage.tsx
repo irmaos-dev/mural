be49d525fed3e2f31033040d1ce2a70a156d94ad
= <=>
2024-09-10 12:38:36 -0300

    deletar client_old

--- client_old/src/components/ContainerPage/ContainerPage.tsx
+++
@@ -1,7 +0,0 @@
-export function ContainerPage({ children }: { children: JSX.Element | JSX.Element[] }) {
-  return (
-    <div className='container page'>
-      <div className='row'> {children} </div>
-    </div>
-  );
-}
--- client_old/src/components/Footer/Footer.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render } from '@testing-library/react';
-import { Footer } from './Footer';
-
-it('Should render', () => {
-  render(<Footer />);
-});
