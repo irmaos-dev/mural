bb96f68d16a629586fbddf8444227d0427d2c37c
= <=>
2024-09-12 13:26:53 -0300

    corrige o test "testUpdateArticleValidationUnique"

--- server/app/Http/Requests/Api/UpdateArticleRequest.php
+++ server/app/Http/Requests/Api/UpdateArticleRequest.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Requests\Api;
+use App\Models\Article;
+use Illuminate\Validation\Rule;
 
 class UpdateArticleRequest extends BaseArticleRequest
 {
@@ -14,9 +16,17 @@ class UpdateArticleRequest extends BaseArticleRequest
 
     public function rules(): array
     {
+        $article = Article::whereSlug($this->route('slug'))
+            ->first();
+
+        $unique = Rule::unique('articles', 'slug');
+        if ($article !== null) {
+            $unique->ignoreModel($article);
+        }
+
         return [
             'title' => ['required_without_all:description,body'],
-            'slug' => ['required_with:title'],
+            'slug' => ['required_with:title', $unique],
             'description' => ['required_without_all:title,body'],
             'body' => ['required_without_all:title,description'],
         ];
