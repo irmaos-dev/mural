925800613e57a6f04554ed5760cfe27af9ae5cfd
daniellacortes <danibcortes@hotmail.com>
2024-10-18 15:02:01 -0300

    Redirecionamento após login

--- client/src/1app/auth.tsx
+++ client/src/1app/auth.tsx
@@ -1,10 +1,13 @@
-import { redirect } from "react-router-dom";
 import { pathKeys } from "~6shared/lib/react-router";
 import { sessionLib, useSessionStore } from "~6shared/session";
 
 export function GoogleLogin() {
 
     const urlParams = JSON.parse(decodeURIComponent(window.location.search.substring(1)).replace(/=([^&]+)\&/g, "=\"$1\",\n\r").replace(/(^|\r)([^=]+)=/g, "\"$2\":").replace(/^/, '{').replace(/$/, '}')) //eslint-disable-line
+
+    if (!Object.keys(urlParams).length){
+        return;
+    }
 
     const { username } = urlParams.user;
 
@@ -13,5 +16,5 @@ export function GoogleLogin() {
     const session = sessionLib.transformUserDtoToSession(urlParams);
     setSession(session);
 
-    redirect(pathKeys.profile.byUsername({ username }));
+    window.location.href = pathKeys.profile.byUsername({ username });
 }
