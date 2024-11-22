9dbe175192a05225a9993846897ac08e967fb778
Force_Six <57117613+moises-elias@users.noreply.github.com>
2024-11-22 13:41:55 -0300

    i72-activitylog  (#90)
    * feat: adiciona laravel-activitylog as dependências
    
    * feat: criado novos arquivos de migrations da para armazenar logs
    
    - cria uma nova tabela no bando de dados para armazenar logs do laravel-activitylog
    
    * feat: cria um novo registro de dados relacionado ao model user
    
    -log a criação de uma novo usuário
    -log no update do perfil do usuário
    -log para exclusão de um usuário (caso esteja adicione essa opção no futuro).
    
    * feat: adiciona id do usuário criado na descrição do log
    
    * feat: cria um novo registro de dados relacionado ao model article
    
    - log a criação de uma novo artigo
    - log no update/edição de um artigo(caso esteja adicione essa opção no futuro).
    - log para exclusão de um artigo, colocando na descrição do log se foi o próprio usuário ou um administrador que excluiu e o nome de quem excluiu.
    
    * feat: cria um novo registro de dados relacionado ao model comment
    
    - log a criação de uma novo comentário em um artigo
    - log no update/edição de um comentário(caso esteja adicione essa opção no futuro).
    - log para exclusão de um comentário, colocando na descrição do log se foi o próprio usuário ou um administrador que excluiu e o nome de quem excluiu.
    
    * feat: adiciona log adicionar ou remover um artigo dos favoritos
    
    - log com nome de FavoriteAction
    - event
     - add : quando usuário adiciona artigo aos favoritos
     - remove : quando usuário artigo remove dos favoritos
    - filtros para consulta:
      - `log_name = 'FavoriteAction'`
      - `event` ('remove' ou 'add')
      - `subject_id` (id do artigo específico)
      - `causer_id` (id do usuário responsável pela ação);
    - Você pode usar a coluna properties também para fazer as consultas:
     - OBS: troque os ids a baixo ao id que você queira consultar.
     - AND properties->>'user_id' = '21'
     - AND properties->>'article_id' = '31';
    
    * feat: adiciona log ao seguir e ao deixar de seguir um usuário
    
    - log com nome de FollowAction
    - event
     - follow : quando usuário começa seguir outro
     - unfollow : quando usuário deixa de seguir outro
    - filtros para consulta:
      - `log_name = 'FollowAction'`
      - `event` ('follow' ou 'unfollow')
      - `subject_id` (id do artigo específico)
      - `causer_id` (id do usuário responsável pela ação);
    - Você pode usar a coluna properties também para fazer as consultas:
     - OBS: troque os ids a baixo ao id que você queira consultar.
     - AND properties->>'user_id' = '21'
     - AND properties->>'article_id' = '31';

--- server/app/Models/Comment.php
+++ server/app/Models/Comment.php
@@ -6,6 +6,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 /**
  * App\Models\Comment
@@ -33,6 +35,7 @@ use Illuminate\Database\Eloquent\Model;
 final class Comment extends Model
 {
     use HasFactory;
+    use LogsActivity;
 
     /**
      * The attributes that are mass assignable.
@@ -42,6 +45,30 @@ final class Comment extends Model
         'author_id',
         'body',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->LogOnly(['*'])
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs()
+            ->useLogName('Comment')
+            ->setDescriptionForEvent(fn (string $eventName) => $this->getDescriptionForEvent($eventName));
+    }
+
+    protected function getDescriptionForEvent(string $eventName): string
+    {
+        $userName = auth()->user()->name ?? 'Desconhecido';
+        $userId = auth()->id();
+        $userOrAdmin = $this->author_id === $userId ? 'pelo próprio dono do artigo:' : 'pelo admin: ';
+
+        return match ($eventName) {
+            'created' => "Um novo comentário foi criado no artigo '{$this->article->title}' de id '{$this->article_id}'. Id do comentário : '{$this->id}'.",
+            'updated' => "O comentário de id '{$this->id}', do artigo de id '{$this->article_id}' foi atualizado {$userOrAdmin} {$userName}.",
+            'deleted' => "O comentário de id '{$this->id}', do artigo de id '{$this->article_id}' foi excluído {$userOrAdmin} {$userName}.",
+            default   => "O comentário de id '{$this->id}', do artigo de id '{$this->article_id}' teve uma ação desconhecida. (ação/evento: {$eventName})"
+        };
+    }
 
     /**
      * Comment's article.
