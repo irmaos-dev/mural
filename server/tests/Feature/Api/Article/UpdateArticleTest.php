00167ef241b6bcf499c233c786e72d24ce913835
MoisesElias123 <moisesfelipedapaz@gmail.com>
2024-12-12 12:38:18 -0300

    test: testando slugs de artigos para garantir comportamento correto do slug ao editar artigo
    - Verifica se o slug é alterado ao editar um artigo
    - Garanti que o slug seja alterado ao editar o título para um título existente
    - Valida erro ao tentar alterar o slug para um slug já existente

--- server/tests/Feature/Api/Article/UpdateArticleTest.php
+++ server/tests/Feature/Api/Article/UpdateArticleTest.php
@@ -219,4 +219,113 @@ final class UpdateArticleTest extends TestCase
             ],
         ];
     }
+
+    public function testUpdateArticleSlugModifier(): void
+    {
+        /** @var User $author */
+        $author = User::factory()->create();
+
+        $articleTitle = $this->faker->sentence(4);
+
+        $response = $this->actingAs($author)->postJson("/api/articles", [
+            "article" => [
+                "title"       => $articleTitle,
+                "image"       => $this->faker->imageUrl(),
+                "description" => $this->faker->paragraph(),
+                "body"        => $this->faker->text(),
+                "tagList"     => [],
+            ],
+        ]);
+
+        $slugOriginal = $response->decodeResponseJson()['article']['slug'];
+
+        $articleTitleEdited = $this->faker->sentence(5);
+        $response2 = $this->actingAs($author)->putJson("/api/articles/{$slugOriginal}", [
+            "article" => [
+                "title" => $articleTitleEdited,
+            ],
+        ]);
+
+        $slugEdited = $response2->decodeResponseJson()['article']['slug'];
+        $this->assertNotEquals($slugOriginal, $slugEdited, 'O slug deve ser modificado após a edição do título.');
+
+    }
+
+    public function testSlugIsUniqueWhenTitleIsDuplicated(): void
+    {
+        /** @var User $author */
+        $author = User::factory()->create();
+
+        $articleTitle = $this->faker->sentence(4);
+
+        $response = $this->actingAs($author)->postJson("/api/articles", [
+            "article" => [
+                "title"       => $articleTitle,
+                "image"       => $this->faker->imageUrl(),
+                "description" => $this->faker->paragraph(),
+                "body"        => $this->faker->text(),
+                "tagList"     => [],
+            ],
+        ]);
+
+        $slug = $response->decodeResponseJson()['article']['slug'];
+
+        $response2 = $this->actingAs($author)->postJson("/api/articles", [
+            "article" => [
+                "title"       => $this->faker->sentence(4),
+                "image"       => $this->faker->imageUrl(),
+                "description" => $this->faker->paragraph(),
+                "body"        => $this->faker->text(),
+                "tagList"     => [],
+            ],
+        ]);
+
+        $slug2 = $response2->decodeResponseJson()['article']['slug'];
+
+        $response3 = $this->actingAs($author)->putJson("/api/articles/{$slug2}", [
+            "article" => [
+                "title" => $articleTitle,
+            ],
+        ]);
+
+        $slug2Edited = $response3->decodeResponseJson()['article']['slug'];
+        $this->assertEquals("{$slug}-2", $slug2Edited);
+    }
+
+    public function testUpdatingSlugToExistingSlugFails(): void
+    {
+        /** @var User $author */
+        $author = User::factory()->create();
+
+        $response = $this->actingAs($author)->postJson("/api/articles", [
+            "article" => [
+                "title"       => $this->faker->sentence(4),
+                "image"       => $this->faker->imageUrl(),
+                "description" => $this->faker->paragraph(),
+                "body"        => $this->faker->text(),
+                "tagList"     => [],
+            ],
+        ]);
+
+        $slug = $response->decodeResponseJson()['article']['slug'];
+
+        $response2 = $this->actingAs($author)->postJson("/api/articles", [
+            "article" => [
+                "title"       => $this->faker->sentence(4),
+                "image"       => $this->faker->imageUrl(),
+                "description" => $this->faker->paragraph(),
+                "body"        => $this->faker->text(),
+                "tagList"     => [],
+            ],
+        ]);
+
+        $slug2 = $response2->decodeResponseJson()['article']['slug'];
+
+        $response3 = $this->actingAs($author)->putJson("/api/articles/{$slug2}", [
+            "article" => [
+                "slug" => $slug,
+            ],
+        ]);
+        $response3->assertStatus(422);
+    }
 }
